e6fac48485bd70a3113d9c10828d34175e8cbe38
alelulich <ale_lulich@hotmail.com>
2023-06-09 17:00:14 +0200

    agregue js

---
+++ script.js
@@ -0,0 +1,31 @@
+alert ("te digo 'hola'");
+let nombre= "ale"
+let apellido= "lulich"
+const mensaje ="este es mi mensaje para 'vos'"
+
+console.log(nombre)
+console.log(apellido)
+console.log(mensaje)
+
+//tipo de datos
+
+let direccion
+console.log(direccion)
+
+//////////////
+
+// let name = prompt ( 'Escribe tu nombre', 'nombre')
+// let lastname = prompt ( 'Escribe tu apellido', 'apellido')
+// let age = prompt ( 'Escribe tu edad', 'edad')
+
+// console.log ('nombre', name)
+// console.log ('apellido', lastname)
+// console.log ('edad', age)
+
+// alert (`Hola ${name} ${lastname} tu edad es ${age}`)
+
+let primerNumero = prompt (' ingrese un valor', '')
+let segundoNumero = prompt ('ingreser otro numero para ser sumado', '')
+let resultado = Number (primerNumero) - Number (segundoNumero);
+
+alert (`el resultado es ${resultado}`)
